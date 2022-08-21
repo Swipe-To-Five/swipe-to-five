ade40a10fc87d4a1ff3f20abe825438f7fa36150
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-08-21 12:17:45 +0530

    Auth and Roles Module Comments

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -3,10 +3,18 @@ import { LoginAccountDto } from './../../dto/auth/login-account.dto';
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
+/**
+ * Controller Layer Implementation for Authentication.
+ */
 @Controller('v1/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  /**
+   * Controller Implementation for account login.
+   * @param loginAccountDto DTO Object for logging into account.
+   * @returns Access and Refresh tokens.
+   */
   @Post('login')
   public async loginUsingCredentials(
     @Body() loginAccountDto: LoginAccountDto,
@@ -14,6 +22,11 @@ export class AuthController {
     return await this.authService.loginUsingCredentials(loginAccountDto);
   }
 
+  /**
+   * Controller Implementation for refreshing tokens.
+   * @param refreshTokenDto DTO Object for tokens refresh
+   * @returns Refreshed access and refresh tokens.
+   */
   @Post('refresh')
   public async refreshAllTokens(
     @Body() refreshTokenDto: RefreshTokenDto,
