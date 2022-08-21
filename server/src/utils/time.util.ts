0e475ee4cd39c49ade6c7118bdb70df2ee5551d6
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-08-21 12:52:40 +0530

    Update time.util.ts

--- server/src/utils/time.util.ts
+++ server/src/utils/time.util.ts
@@ -1,3 +1,7 @@
+/**
+ * Fetch date one month later.
+ * @returns Date One Month Later
+ */
 export const getOneMonthLater = (): Date => {
   const today = new Date(Date.now());
   today.setMonth(today.getMonth() + 1);
