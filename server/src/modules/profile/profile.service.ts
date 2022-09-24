f3ac3221d12ccc5b825ab6c37a4d5a70c97ccaba
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-09-24 20:40:42 +0530

    Service Implementation for fetching recruitee / recruiter profile. Closes #47 and #45

--- server/src/modules/profile/profile.service.ts
+++ server/src/modules/profile/profile.service.ts
@@ -1,10 +1,11 @@
+import { RECRUITEE, RECRUITER } from './../../constants/roles.constant';
 import { UpdateRecruiterProfile } from './../../dto/profile/update-recruiter-profile.dto';
 import { UpdateRecruiteeProfile } from './../../dto/profile/update-recruitee-profile.dto';
 import { Account } from './../../models/account.model';
 import { Skill } from './../../models/skill.model';
 import { RecruiterProfile } from './../../models/recruiter-profile.model';
 import { RecruiteeProfile } from './../../models/recruitee-profile.model';
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 
 @Injectable()
@@ -19,6 +20,25 @@ export class ProfileService {
     @InjectModel(Skill)
     private readonly skillRepository: typeof Skill,
   ) {}
+
+  /**
+   * Service Implementation for fetching recruiter / recruitee profile.
+   * @param account Logged In Account
+   * @returns Recruitee or Recruiter profile
+   */
+  public async fetchProfile(
+    account: Account,
+  ): Promise<RecruiteeProfile | RecruiterProfile> {
+    return account.role === RECRUITEE
+      ? await this.recruiteeProfileRepository.findOne({
+          where: { accountId: account.id },
+        })
+      : account.role === RECRUITER
+      ? await this.recruiterProfileRepository.findOne({
+          where: { accountId: account.id },
+        })
+      : null;
+  }
 
   /**
    * Service Implementation for updating recruitee profile.
